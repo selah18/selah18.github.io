c25d86a11f16987303b7358515264482df3e3fac
Selahadin Yasin <selahadinosman1@gmail.com>
2020-04-12 08:38:54 -0700

    f

--- example1-JsNgram.js
+++ example1-JsNgram.js
@@ -40,8 +40,8 @@ $(document).ready(function(){
     }
   });
   
-  $q.focus();
-});
+
+
 
 $('#error').click(function(){
   
@@ -66,22 +66,6 @@ var chart = new CanvasJS.Chart("chartContainer", {
     color: "#014D65",
     dataPoints: [
       { y: 3, label: "Sweden" },
-      { y: 7, label: "Taiwan" },
-      { y: 5, label: "Russia" },
-      { y: 9, label: "Spain" },
-      { y: 7, label: "Brazil" },
-      { y: 7, label: "India" },
-      { y: 9, label: "Italy" },
-      { y: 8, label: "Australia" },
-      { y: 11, label: "Canada" },
-      { y: 15, label: "South Korea" },
-      { y: 12, label: "Netherlands" },
-      { y: 15, label: "Switzerland" },
-      { y: 25, label: "Britain" },
-      { y: 28, label: "Germany" },
-      { y: 29, label: "France" },
-      { y: 52, label: "Japan" },
-      { y: 103, label: "China" },
       { y: 134, label: "US" }
     ]
   }]
@@ -89,3 +73,16 @@ var chart = new CanvasJS.Chart("chartContainer", {
 chart.render();
 
 }
+
+
+
+
+
+
+
+
+
+
+  $q.focus();
+});
+
