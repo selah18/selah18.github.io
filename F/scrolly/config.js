aeaae8a330b714bcae1f4c112b9e0cd302df2db8
Selahadin Yasin <selahadinosman1@gmail.com>
2020-04-21 18:33:02 -0700

    h

---
+++ F/scrolly/config.js
@@ -0,0 +1,137 @@
+var config = {
+    style: 'mapbox://styles/selahadinosma/ck98ydjax07d61io5ak71ev6v',
+    accessToken: 'pk.eyJ1Ijoic2VsYWhhZGlub3NtYSIsImEiOiJjazN0bWd5MnIwMXpoM2Rxc25vYTh2cWZwIn0.jLJ0WYJz1asf5kJa80iCBg',
+    showMarkers: true,
+    theme: 'dark',
+    alignment: 'center',
+    title: 'SEX IN THE SHUTTLE?',
+    subtitle: 'A descriptive and interesting subtitle to draw in the reader',
+    byline: 'By SELAH',
+    footer: 'Source: source citations, etc.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            title: 'Intro',
+            // image: 'images/asrbeamst.png',
+            description: '<iframe width="550" height="255" src="https://www.youtube.com/embed/ocFHDEINd5s?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            location: {
+                center: [77.09546, 28.55204],
+                zoom: 16.00,
+                pitch: 60.00,
+                bearing: -79.20
+            },
+            onChapterEnter: [
+       
+            ],
+            onChapterExit: [
+        
+            ]
+        },
+        {
+            id: 'firstvictim',
+            title: 'Story in short from the victim',
+            // image: './path/to/image/source.png',
+            description: '<iframe width="560" height="315" src="https://www.youtube.com/embed/iVLHUgKyoII?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            location: {
+center: [-18.16714, 51.99837],
+zoom: 4.48,
+pitch: 0.00,
+bearing: -7.20
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },        {
+            id: 'secondvictim',
+            title: 'Story from the other victim',
+            // image: './path/to/image/source.png',
+            description: '<iframe width="560" height="315" src="https://www.youtube.com/embed/cvS_YAhFk2w?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            location: {
+center: [76.86667, 28.61385],
+zoom: 9.75,
+pitch: 0.00,
+bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },        {
+            id: 'suspensionfromashoka',
+            title: 'Suspension letter from Ashoka University',
+            image: 'images/ashoka.png',
+            description: '',
+            location: {
+center: [77.09617, 28.94570],
+zoom: 15.95,
+pitch: 0.00,
+bearing: -10.27
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },        {
+            id: 'studentsreplyfacebook',
+            title: 'Ashokans Reaction',
+            image: 'images/ashstu.png',
+            description: '',
+            location: {
+center: [77.09982, 28.94601],
+zoom: 17.19,
+pitch: 58.00,
+bearing: -15.87
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },        
+        {
+            id: 'sheffieldsreply',
+            title: 'Letter From University of Sheffield',
+            image: 'images/shefoka.png',
+            description: '',
+            location: {
+center: [-1.50183, 53.38194],
+zoom: 17.41,
+pitch: 53.50,
+bearing: -11.24
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },        {
+            id: 'hisparentsonfacebook',
+            title: 'His Family and Close Friends Reaction',
+            image: 'images/shfstu.png',
+            description: '',
+            location: {
+center: [-4.57097, 53.33911],
+zoom: 7.53,
+pitch: 60.00,
+bearing: -7.20
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },        {
+            id: 'vc',
+            title: 'The VC Reply to my Email',
+            image: './path/to/image/source.png',
+            description: '',
+            location: {
+center: [77.07406, 28.94007],
+zoom: 13.88,
+pitch: 60.00,
+bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },        {
+            id: 'hisgoodbye',
+            title: 'Farewell Message From the Victim',
+            // image: './path/to/image/source.png',
+            description: '<iframe width="560" height="315" src="https://www.youtube.com/embed/FH-NFy6k7ag?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+            location: {
+center: [77.08417, 28.55530],
+zoom: 15.10,
+pitch: 60.00,
+bearing: 0.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
